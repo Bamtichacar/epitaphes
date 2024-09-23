2dc8788e92c9ff041d5b0e93c705d23c3f86b57f
Lauliek <laureline.pellegrini@gmail.com>
2024-09-23 12:18:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+une navigation (menu) avec des liens vers les 3 pages suivantes : Accueil,Produits,Contact 
+ une bas de page avec l'adresse et le mail de la société fictive 
+ Ces deux premiers éléments doivent se retrouver systématiquement sur toutes les pages  
+ */
+
